1a528b4df96462b03cda00cce6945539fc1d314f
lvlahraam <lvlahraam@gmail.com>
2021-11-04 18:05:45 +0330

    Update utility.py

--- core/commands/utility.py
+++ core/commands/utility.py
@@ -82,7 +82,7 @@ class Utility(commands.Cog, description="Useful stuff that are open to everyone"
     # Notes-Add
     @notes.command(name="add", aliases=["+"], help="Adds the given note to your notes")
     async def notes_add(self, ctx:commands.Context, *, note:str):
-        notes = await self.bot.postgres.fetch("SELECT * FROM notes WHERE user_id=$2", ctx.author.id)
+        notes = await self.bot.postgres.fetch("SELECT * FROM notes WHERE user_id=$1", ctx.author.id)
         noteaddmbed = discord.Embed(
             color=self.bot.color,
             timestamp=ctx.message.created_at
