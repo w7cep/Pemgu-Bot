9be1344d043a2daf86bd87fce40055d348a7541a
lvlahraam <lvlahraam@gmail.com>
2021-11-12 04:38:53 +0330

    Update error.py

--- core/events/error.py
+++ core/events/error.py
@@ -7,42 +7,14 @@ class OnError(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
+    async def on_command_completion(self, ctx:commands.Context):
+        await ctx.message.add_reaction("✔️")
+
+    @commands.Cog.listener()
     async def on_command_error(self, ctx:commands.Context, error):
+        await ctx.message.add_reaction("❌")
         if isinstance(error, commands.CommandInvokeError):
             error = error.original
-        if isinstance(error, commands.CheckFailure):
-            cfmbed = discord.Embed(
-                color=self.bot.color,
-                title=F"❌ An Error Occurred: {ctx.command}",
-                description=F"```diff\n{error.message}\n```",
-                timestamp=ctx.message.created_at
-            )
-            cfmbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
-            return await ctx.send(embed=cfmbed)
-        elif isinstance(error, commands.CommandNotFound):
-            cmd = ctx.invoked_with
-            cmds = [cmd.name for cmd in self.bot.commands]
-            matches = difflib.get_close_matches(cmd, cmds)
-            matcnfmbed = discord.Embed(
-                color=self.bot.color,
-                title=F"Couldn't find command called: {cmd}.",
-                description=F"Maybe you meant:\n{' - '.join([match for match in matches])}",
-                timestamp=ctx.message.created_at
-            )
-            matcnfmbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
-            nmatcnfmbed = discord.Embed(
-                color=self.bot.color,
-                title=F"Couldn't find command called: {cmd}.",
-                description=F"Use help command to know what command you're looking for",
-                timestamp=ctx.message.created_at
-            )
-            nmatcnfmbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
-            if len(matches) > 0:
-                view = dymview.DYMView(ctx, matches)
-                view.message = await ctx.send(embed=matcnfmbed)
-                return
-            else:
-                return await ctx.send(embed=nmatcnfmbed)
         print("".join(traceback.format_exception(type(error), error,  error.__traceback__)))
         errormbed = discord.Embed(
             color=self.bot.color,
