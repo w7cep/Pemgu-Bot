403de3fc150eb6804ee7076523979bc78759edf0
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-11-14 01:02:57 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,7 +56,7 @@ async def create_node_pomice():
         print(F"Created a Pomice Node: {n} - {dnodes.get(n)['identifier']}")
     for s in slnodes:
         await bot.pomice.create_node(bot=bot, secure=True, host=s, port=443, password=sdnodes.get(s)["pw"], identifier=sdnodes.get(s)["identifier"], spotify_client_id=os.getenv("SPOTIFY").split(", ")[0], spotify_client_secret=os.getenv("SPOTIFY").split(", ")[1])
-        print(F"Created a Secure Pomice Node: {s} - {sdnodes.get(s)['identifier']")
+        print(F"Created a Secure Pomice Node: {s} - {sdnodes.get(s)['identifier']}")
 
 class PemguBase(commands.AutoShardedBot):
     def __init__(self, **kwargs):
