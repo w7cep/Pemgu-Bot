91f8672496bbd54b9f967c06923453a762d04f88
lvlahraam <lvlahraam@gmail.com>
2021-11-20 07:15:35 +0330

    .

--- core/views/confirm.py
+++ core/views/confirm.py
@@ -23,12 +23,10 @@ class Confirm(discord.ui.View):
     async def on_timeout(self):
         if self.children:
             self.clear_items()
-            await self.message.edit(view=self)
+            await self.message.edit(content="Timed-out", view=self)
     
     async def interaction_check(self, interaction:discord.Interaction):
-        if interaction.user.id == self.ctx.message.author.id:
-            return True
-        else:
+        if interaction.user.id != self.ctx.message.author.id:
             icheckmbed = discord.Embed(
                 color=self.ctx.bot.color,
                 title=F"You can't use this",
@@ -38,3 +36,4 @@ class Confirm(discord.ui.View):
             icheckmbed.set_author(name=interaction.user, icon_url=interaction.user.display_avatar.url)
             await interaction.response.send_message(embed=icheckmbed, ephemeral=True)
             return False
+        return True
--- core/views/pagination.py
+++ core/views/pagination.py
@@ -30,10 +30,13 @@ class ViewPagination(discord.ui.View):
             embed = self.embeds[self.embed]
         await interaction.response.edit_message(embed=embed, view=button.view)
 
+    async def on_timeout(self):
+        if self.children:
+            self.clear_items()
+            await self.message.edit(content="Timed-out", view=self)
+
     async def interaction_check(self, interaction:discord.Interaction):
-        if interaction.user.id == self.ctx.message.author.id:
-            return True
-        else:
+        if interaction.user.id != self.ctx.message.author.id:
             icheckmbed = discord.Embed(
                 color=self.ctx.bot.color,
                 title=F"You can't use this",
@@ -43,3 +46,4 @@ class ViewPagination(discord.ui.View):
             icheckmbed.set_author(name=interaction.user, icon_url=interaction.user.display_avatar.url)
             await interaction.response.send_message(embed=icheckmbed, ephemeral=True)
             return False
+        return True
