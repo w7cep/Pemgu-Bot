91b2029ae4829a9664ecc86fcc07c04df3d65c0b
lvlahraam <lvlahraam@gmail.com>
2021-11-21 12:52:04 +0330

    Update information.py

--- core/commands/information.py
+++ core/commands/information.py
@@ -361,8 +361,8 @@ class Information(commands.Cog, description="Stalking people is wrong and bad!")
         for emoji in ctx.guild.emojis:
             if name:
                 if name in emoji.name.lower(): ps.append(emoji)
+                else: return await ctx.send(F"Couldn't find any emoji with {name}")
             else: ps.append(emoji)
-        if not ps: return await ctx.send(F"Couldn't find any emoji with {name}")
         es = []
         for p in ps:
             ei = [
