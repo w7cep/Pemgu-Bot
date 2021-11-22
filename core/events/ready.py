9304537d86724e5048c0d0559d07156cfda32274
lvlahraam <lvlahraam@gmail.com>
2021-11-22 07:18:09 +0330

    Update ready.py

--- core/events/ready.py
+++ core/events/ready.py
@@ -8,13 +8,13 @@ class OnReady(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         self.bot.uptime = discord.utils.utcnow()
-        oni = [
+        ori = [
             F"Logged in as: {self.bot.user} - {self.bot.user.id}",
             F"Main prefix is: {self.bot.default_prefix}",
             F"Servers bot is in: {len(self.bot.guilds)}",
             "The Bot is online now"
         ]
-        print("\n".join(o for o in oni))
+        print("\n".join(o for o in ori))
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name=F"{len(self.bot.guilds)} and {len(self.bot.users)}"))
 
 def setup(bot):
