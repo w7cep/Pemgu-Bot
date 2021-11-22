d98705f64f3dfcee86def308ec4e505ce5c1b899
lvlahraam <lvlahraam@gmail.com>
2021-11-22 07:35:37 +0330

    Update connection.py

--- core/events/connection.py
+++ core/events/connection.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
 
-class OnReady(commands.Cog):
+class OnConnect(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_connect(self):
         self.bot.uptime = discord.utils.utcnow()
-        ori = [
+        oni = [
             F"Logged in as: {self.bot.user} - {self.bot.user.id}",
             F"Main prefix is: {self.bot.default_prefix}",
             F"Servers bot is in: {len(self.bot.guilds)}",
             F"Users bot is seeing: {len(self.bot.users)}",
         ]
-        print("\n".join(o for o in ori))
+        print("\n".join(o for o in oni))
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.competing, name=F"{len(self.bot.guilds)} and {len(self.bot.users)}"))
 
 class OnDisconnect(commands.Cog):
@@ -28,5 +28,5 @@ class OnDisconnect(commands.Cog):
             await self.bot.session.close()
 
 def setup(bot):
-    bot.add_cog(OnReady(bot))
+    bot.add_cog(OnConnect(bot))
     bot.add_cog(OnDisconnect(bot))
