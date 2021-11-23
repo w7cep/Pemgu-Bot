984bb9b3345a115ef3af0d62669fc3f392087f35
lvlahraam <lvlahraam@gmail.com>
2021-11-23 08:31:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,55 +28,8 @@ async def create_session_aiohttp():
 async def create_node_pomice():
     await bot.wait_until_ready()
     bot.pomice = pomice.NodePool()
-    # lnodes = [
-    #     "lavalinknl.ml",
-    #     "unit1.nighthost.tech",
-    #     "lavalink.darrennathanael.com",
-    #     "lava.link",
-    #     "node1.cjstevenson.com"
-    # ]
-    # dnodes = {
-    #     "lavalinknl.ml": {"port": "2333", "pw": "Raccoon", "identifier": "lavalinknl.ml"},
-    #     "unit1.nighthost.tech": {"port": "23549", "pw": "emilylava", "identifier": "unit1.nighthost.tech"},
-    #     "lavalink.darrennathanael.com": {"port": "80", "pw": "clover", "identifier": "lavalink.darrennathanael.com"},
-    #     "lava.link": {"port": "80", "pw": "quartz", "identifier": "lava.link"},
-    #     "node1.cjstevenson.com": {"port": "25503", "pw": "lookbehindyou", "identifier": "node1.cjstevenson.com"}
-    # }
-    slnodes = [
-        "lavalink.devz.cloud",
-        "lavalink2.devz.cloud",
-        "lavalink.devin-dev.xyz",
-        "lava.devin-dev.xyz",
-        "disbotlistlavalink.ml",
-        "www.lavalink-priyam.ml",
-        "lavalink-replit.rainandrewandre.repl.co"
-    ]
-    sdnodes = {
-        "lavalink.devz.cloud": {"pw": "mathiscool", "identifier": "lavalink.devz.cloud"},
-        "lavalink2.devz.cloud": {"pw": "mathiscool", "identifier": "lavalink2.devz.cloud"},
-        "lavalink.devin-dev.xyz": {"pw": "lava123", "identifier": "lavalink.devin-dev.xyz"},
-        "lava.devin-dev.xyz": {"pw": "lava123", "identifier": "lava.devin-dev.xyz"},
-        "disbotlistlavalink.ml": {"pw": "LAVA", "identifier": "disbotlistlavalink.ml"},
-        "www.lavalink-priyam.ml": {"pw": "methisbigbrain", "identifier": "lavalink-priyam.ml"},
-        "lavalink-replit.rainandrewandre.repl.co": {"pw": "maybeiwasboring", "identifier": "lavalink-replit.rainandrewandre.repl.co"}
-    }
-    await bot.pomice.create_node(bot=bot, secure=True, host="www.lavalink-priyam.ml", port=443, password="methisbigbrain", identifier="lavalink-priyam.ml", spotify_client_id=os.getenv("SPOTIFY").split(", ")[0], spotify_client_secret=os.getenv("SPOTIFY").split(", ")[1])
-    # print("UnSecure Nodes")
-    # for n in lnodes:
-    #     try:
-    #         await bot.pomice.create_node(bot=bot, host=n, port=dnodes.get(n)["port"], password=dnodes.get(n)["pw"], identifier=dnodes.get(n)["identifier"], spotify_client_id=os.getenv("SPOTIFY").split(", ")[0], spotify_client_secret=os.getenv("SPOTIFY").split(", ")[1])
-    #         print(F"Created a Pomice Node: {n} - {dnodes.get(n)['identifier']}")
-    #     except Exception as e:
-    #         print(e)
-    #         pass
-    # print("Secure Nodes")
-    # for s in slnodes:
-    #     try:
-    #         await bot.pomice.create_node(bot=bot, secure=True, host=s, port=443, password=sdnodes.get(s)["pw"], identifier=sdnodes.get(s)["identifier"], spotify_client_id=os.getenv("SPOTIFY").split(", ")[0], spotify_client_secret=os.getenv("SPOTIFY").split(", ")[1])
-    #         print(F"Created a Secure Pomice Node: {s} - {sdnodes.get(s)['identifier']}")
-    #     except Exception as e:
-    #         print(e)
-    #         pass
+    await bot.pomice.create_node(bot=bot, host="unit1.nighthost.tech", port="23549", password="emilylava", identifier="unit1.nighthost.tech", spotify_client_id=os.getenv("SPOTIFY").split(", ")[0], spotify_client_secret=os.getenv("SPOTIFY").split(", ")[1])
+    print("Created a Pomice Node")
 
 class PemguBase(commands.AutoShardedBot):
     def __init__(self, **kwargs):
